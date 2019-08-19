da610a35956270e2c5756618cddf8296bff722c3
sahilgupta1801 <46751362+sahilgupta1801@users.noreply.github.com>
2019-08-19 15:39:34 +0530

    Create Client.c

---
+++ Computer Networks/Lab2/Q4/Client.c
@@ -0,0 +1,26 @@
+void PerformClientTask(int* sockfd){
+	int n = 1;
+	char ch;
+    int val1, val2;
+    char buf[256];
+        printf("Enter Value 1: ");
+        scanf("%d", &val1);
+        printf("Enter Value 2: ");
+        scanf("%d", &val2);
+        while ((getchar()) != '\n'); 
+		printf("Enter Operand (+ - * / ) : ");
+        scanf("%c", &ch);
+        int convertedVal1 = htonl(val1);
+        int convertedVal2 = htonl(val2);
+        write(*sockfd, &convertedVal1, sizeof(convertedVal1));
+		write(*sockfd, &convertedVal2, sizeof(convertedVal2));
+        write(*sockfd, &ch, sizeof(ch));
+		n = read(*sockfd, buf, sizeof(buf));
+		puts(buf);
+}
+
+int main(){
+	int sockfd;
+	sockfd = CreateClientSocket();
+	PerformClientTask(&sockfd);
+}
