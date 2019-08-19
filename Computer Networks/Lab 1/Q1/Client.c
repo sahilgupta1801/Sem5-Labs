78571777205b2f3cce4e93b07bcb49d88e672c7e
sahilgupta1801 <46751362+sahilgupta1801@users.noreply.github.com>
2019-08-19 15:30:33 +0530

    Create Client.c

---
+++ Computer Networks/Lab 1/Q1/Client.c
@@ -0,0 +1,37 @@
+int CreateClientSocket(){
+	int len, sockfd, result;
+	struct sockaddr_in serverAddress;
+	sockfd = createSocketFileDescriptor();
+	serverAddress = createSocketWithAddress();
+	len = sizeof(serverAddress);
+	result = connect(sockfd, (struct sockaddr *)&serverAddress, len);
+	if (result == -1){
+		perror("CLIENT ERROR");
+		exit(1);
+	}
+	return sockfd;
+}
+
+void PerformClientTask(int sockfd){
+	int n = 1;
+	char ch[256], buf[256];
+
+	while(1){
+		n = 1;
+		printf("Enter String: ");
+		gets(ch);
+		ch[strlen(ch)] = '\0';
+		write(sockfd, ch, sizeof(ch));
+		printf("Respone from the server is... ");
+		while(n){
+			n = read(sockfd, buf, sizeof(buf));
+			puts(buf);
+		}
+	}
+}
+
+int main(){
+	int sockfd;
+	sockfd = CreateClientSocket();
+	PerformClientTask(sockfd);
+}
